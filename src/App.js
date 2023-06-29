24261605788c1585a8b21cc3989e30d9601f771c
Argha Paul <arghapaul002@gmail.com>
2023-06-29 17:39:07 +0530

    Dark mode functionality added in navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "rgb(25 39 58)";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+    } 
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container">
-        {/* <TextForm heading="Enter the Text" /> */}
-        <About />
+        <TextForm heading="Enter the Text" mode={mode} />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,13 +26,14 @@ const handleOnChange = (event) => {
 setText(event.target.value);
 };
 
-const [text, setText] = useState("Enter the Text");
+const [text, setText] = useState("");
 
 return (
-<div>
-  <h1>{props.heading}</h1>
+<div style={{color: props.mode === 'dark'?'white':'black'}}>
+  <h1 >{props.heading}</h1>
   <div className="mb-3">
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" cols="30" rows="10"></textarea>
+    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#c5c5c5':'white'}} id="myBox" cols="30" rows="10">
+    </textarea>
   </div>
   <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
   <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase</button>
@@ -42,6 +43,9 @@ return (
   <div className="container">
     <h2>Results</h2>
     <p>Total {text.split(" ").length} words and {text.length} character</p>
+    <p>{0.08 * text.split(" ").length} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text.length > 0?text:'Enter something in the above textbox to preview it here'}</p>
   </div>
 </div>
 );
