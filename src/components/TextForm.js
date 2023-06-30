021653e1bffd9b21b067f692a29578603e277c38
Argha Paul <arghapaul002@gmail.com>
2023-06-30 19:32:12 +0530

    All small bugs resolved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,7 @@ const handleCopy = () => {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Text copied to clipboard","success");
    };
 
@@ -40,23 +41,23 @@ const [text, setText] = useState("");
 
 return (
 <div style={{color: props.mode === 'dark'?'white':'black'}}>
-  <h1 >{props.heading}</h1>
+  <h1 className="my-2">{props.heading}</h1>
   <div className="mb-3">
     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#c5c5c5':'white'}} id="myBox" cols="30" rows="10">
     </textarea>
   </div>
-  <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
-  <button className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert to Lowercase</button>
-  <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-  <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
-  <button className="btn btn-primary mx-2 my-2" onClick={clearClick}>Clear Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLowerClick}>Convert to Lowercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={clearClick}>Clear Text</button>
 
   <div className="container">
     <h2>Results</h2>
     <p>Total {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} character</p>
-    <p>{0.08 * text.split(" ").length} Minutes read</p>
+    <p>{0.08 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
     <h2>Preview</h2>
-    <p>{text.length > 0?text:'Enter something in the above textbox to preview it here'}</p>
+    <p>{text.length > 0?text:'Nothing to Preview!'}</p>
   </div>
 </div>
 );
